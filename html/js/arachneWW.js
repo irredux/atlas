8a478b4b47051da293d648dab559c3b5373c1e71
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2021-06-06 21:58:41 +0200

    Beta 7.3

--- html/js/arachneWW.js
+++ html/js/arachneWW.js
@@ -214,6 +214,7 @@ class ArachneDatabase{
                     headers: {"Authorization": `Bearer ${this.token}`}
                 })
                     .then(response => {
+                        if(!response.ok){throw "ERROR: connection to server lost."}
                         let count = 0;
                         let restOfChunk = "";
                         let decoder = new TextDecoder();
@@ -254,7 +255,7 @@ class ArachneDatabase{
                                         }
                                         if(items.length > 0){controller.enqueue(items)}
                                         pump();
-                                    }).catch(e => {throw "connection lost. "+e});
+                                    });
                                 }
                                 pump();
                             }
