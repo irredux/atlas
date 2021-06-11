999f071cef4183453257cd66176f23568856c2ee
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2021-06-11 14:46:12 +0200

    Beta 8.3

--- dMLW/buticula/buticula.py
+++ dMLW/buticula/buticula.py
@@ -196,15 +196,15 @@ class Buticula(Bottle):
     def create_mlw_file(self, i_data):
         with open(self.p + "/MLW-Software/input.mlw", "wb") as i_file:
             i_file.write(i_data)
-        #server_status = subprocess.run(f"python {self.p}/MLW-Software/MLWServer.py --status", shell=True)
+        #server_status = subprocess.run(f"python3 {self.p}/MLW-Software/MLWServer.py --status", shell=True)
         #print("SERVER STATUS:", server_status)
-        #subprocess.run(f"python {self.p}/MLW-Software/MLWServer.py --port 9997 --startserver&", shell=True)
+        #subprocess.run(f"python3 {self.p}/MLW-Software/MLWServer.py --port 9997 --startserver&", shell=True)
         if path.exists(self.p + "/MLW-Software/Ausgabe"):
             rmtree(self.p + "/MLW-Software/Ausgabe");
         subprocess.run(
-                f"python {self.p}/MLW-Software/MLWServer.py --port 9997 {self.p}/MLW-Software/input.mlw",
+                f"python3 {self.p}/MLW-Software/MLWServer.py --port 9997 {self.p}/MLW-Software/input.mlw",
                 shell=True)
-        subprocess.run(f"python {self.p}/MLW-Software/MLWServer.py --port 9997 --stopserver", shell=True)
+        subprocess.run(f"python3 {self.p}/MLW-Software/MLWServer.py --port 9997 --stopserver", shell=True)
         o_data = {}
         with open(self.p + "/MLW-Software/Ausgabe/HTML-Vorschau/input.html", "r") as html_file:
             o_data["html"] = html_file.read()
