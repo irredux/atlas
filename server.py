a1fc57e077ce2a2e7cc841f889aa4eb1c0d25055
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2021-12-16 16:08:52 +0100

    Beta 11.9

--- server.py
+++ server.py
@@ -691,9 +691,12 @@ def setup_MGH_ressource(basic_url, ressource_path, edition_id, pages_lst):
         db.save("scan_lnk", {"edition_id": new_res_id, "scan_id": new_img["id"]})
 
 def import_mgh2():
+    # F.-J. Schmale, Adalbertus Samaritanus, Praecepta dictaminum (MG Quellen z. Geistesgesch. d. Mittelalters III). 1961 <- welche seiten?
+    # K. Hampe, Die Aktenstücke z. Frieden von S. Germano (MG Epist. sel. IV). 1926 -> Welche Seiten?
+    # T. Schieffer, Die Urkunden der burgundischen Rudolfinger (MG regum Burgund. e stirpe Rudolf. diplomata et acta). 1977 -> Welche Seiten? https://www.dmgh.de/mgh_dd_rudolf/index.htm#page/92/mode/1up
     # F. Güterbock (MG Script. rer. Germ. N. S. VII)
     setup_MGH_ressource( # ACERB. MOR. hist. 130–76
-        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/150/mgh_ss_rer_germ_n_s_7_",
+        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/300/mgh_ss_rer_germ_n_s_7_",
         "/A/ACERB. MOR. hist. (ed. Güterbock)/",
         2068,
         [
@@ -701,7 +704,7 @@ def import_mgh2():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # HIST. Frid. imp. cont. 177–218
-        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/150/mgh_ss_rer_germ_n_s_7_",
+        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/300/mgh_ss_rer_germ_n_s_7_",
         "/H/HIST. Frid. imp. cont. (ed. Güterbock)/",
         2494,
         [
@@ -709,7 +712,7 @@ def import_mgh2():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # HIST. Frid. imp. retr. 1–218 # warum hier auch 218???
-        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/150/mgh_ss_rer_germ_n_s_7_",
+        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/300/mgh_ss_rer_germ_n_s_7_",
         "/H/HIST. Frid. imp. retr. (ed. Güterbock)/",
         2495,
         [
@@ -717,7 +720,7 @@ def import_mgh2():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # OTTO MOR. hist. 1–129 # siehe oben???
-        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/150/mgh_ss_rer_germ_n_s_7_",
+        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/300/mgh_ss_rer_germ_n_s_7_",
         "/O/OTTO MOR. hist. (ed. Güterbock)/",
         2631,
         [
@@ -725,10 +728,133 @@ def import_mgh2():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     
-def import_mgh():
+def import_mgh3():
+    # O. Holder-Egger MG Script. XV (1887)
+    setup_MGH_ressource( # ADREV. Bened. 478–97
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/A/ADREV. Bened. (ed. Holder-Egger)/",
+        2074,
+        [
+            {"img_number": 486, "page_number": 478, "count": 20},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # BOVO SITH. Bert. 525–34
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/B/BOVO SITH. Bert. (ed. Holder-Egger)/",
+        2892,
+        [
+            {"img_number": 533, "page_number": 525, "count": 10},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # ERMENR. Sval. 153–63
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/E/ERMENR. Sval. (ed. Holder-Egger)/",
+        2369,
+        [
+            {"img_number": 161, "page_number": 153, "count": 11},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # FOLCUIN. vita Folcuin. 424–30
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/F/FOLCUIN. vita Folcuin. (ed. Holder-Egger)/",
+        2380,
+        [
+            {"img_number": 432, "page_number": 424, "count": 7},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # HUGEB. Willib. 86–106
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/H/HUGEB. Willib. (ed. Holder-Egger)/",
+        2531,
+        [
+            {"img_number": 94, "page_number": 86, "count": 21},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # LIUTG. Greg. 66–79
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/L/LIUTG. Greg. (ed. Holder-Egger)/",
+        2587,
+        [
+            {"img_number": 73, "page_number": 66, "count": 14},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # LUP. FERR. Wigb. 37–43
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/L/LUP. FERR. Wigb. (ed. Holder-Egger)/",
+        2595,
+        [
+            {"img_number": 44, "page_number": 37, "count": 7},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # MEGINH. BLEID. Ferr. 149–50
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/M/MEGINH. BLEID. Ferr. (ed. Holder-Egger)/",
+        2603,
+        [
+            {"img_number": 157, "page_number": 149, "count": 2},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # ODBERT. Frid. 344–56
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/L/LUP. FERR. Wigb. (ed. Holder-Egger)/",
+        2603,
+        [
+            {"img_number": 157, "page_number": 149, "count": 2},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    # B. Schmeidler, Adam von Bremen, Hamburgische Kirchengesch. (MG Script. rer. Germ.). 1917.
+    setup_MGH_ressource( # ADAM gest. 1–280
+        "https://www.dmgh.de/mgh_ss_rer_germ_2/img/300/mgh_ss_rer_germ_2_",
+        "/A/ADAM gest. (ed. Schmeidler)/",
+        2073,
+        [
+            {"img_number": 68, "page_number": 1, "count": 280},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # ADAM gest. epil. 281–83
+        "https://www.dmgh.de/mgh_ss_rer_germ_2/img/300/mgh_ss_rer_germ_2_",
+        "/A/ADAM gest. (epil.) (ed. Schmeidler)/",
+        2959,
+        [
+            {"img_number": 348, "page_number": 1, "count": 3},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # SCHOL. Adam gest. (in notis) 4–274
+        "https://www.dmgh.de/mgh_ss_rer_germ_2/img/300/mgh_ss_rer_germ_2_",
+        "/S/SCHOL. Adam gest. (in notis) (ed. Schmeidler)/",
+        2679,
+        [
+            {"img_number": 71, "page_number": 4, "count": 271},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    # F. Kurze, Reginonis ... chronicon (MG Script. rer. Germ.). 1890. p. 154-79
+    setup_MGH_ressource( # ADALB. MAGD. chron. 154–79
+        "https://www.dmgh.de/mgh_ss_rer_germ_50/img/300/mgh_ss_rer_germ_50_",
+        "/A/ADALB. MAGD. chron. (ed. Kurze)/",
+        2071,
+        [
+            {"img_number": 173, "page_number": 154, "count": 26},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # REGINO chron. 1–153
+        "https://www.dmgh.de/mgh_ss_rer_germ_50/img/300/mgh_ss_rer_germ_50_",
+        "/R/REGINO chron. (ed. Kurze)/",
+        2655,
+        [
+            {"img_number": 20, "page_number": 1, "count": 153},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # REGINO chron. app. 180
+        "https://www.dmgh.de/mgh_ss_rer_germ_50/img/300/mgh_ss_rer_germ_50_",
+        "/R/REGINO chron. (app.) (ed. Kurze)/",
+        2958,
+        [
+            {"img_number": 199, "page_number": 180, "count": 1},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
     # P. v. Winterfeld MG Poet. IV (1899)
     setup_MGH_ressource( # ARBO SANGERM. bell. 77–122
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/A/ARBO SANGERM. bell (ed. v. Winterfeld)/",
         2067,
         [
@@ -736,7 +862,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # CARM. de Bened. 209–31
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/C/CARM. de Bened. (ed. v. Winterfeld)/",
         2954,
         [
@@ -744,7 +870,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # CARM. de Cass. 181–96
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/C/CARM. de Cass. (ed. v. Winterfeld)/",
         2955,
         [
@@ -752,7 +878,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # CARM. de Lamb. 142–59
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/C/CARM. de Lamb. (ed. v. Winterfeld)/",
         2207,
         [
@@ -760,7 +886,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # CARM. de Quint. 197–208
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/C/CARM. de Quint. (ed. v. Winterfeld)/",
         2216,
         [
@@ -768,7 +894,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # CARM. de Quint. 197–208
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/C/CARM. de Quint. (ed. v. Winterfeld)/",
         2216,
         [
@@ -776,7 +902,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # EUGEN. VULG. syll. 412–40
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/E/EUGEN. VULG. syll. (ed. v. Winterfeld)/",
         2373,
         [
@@ -784,7 +910,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # EUGEN. VULG. syll. (app.) 441–44
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/E/EUGEN. VULG. syll. (app.) (ed. v. Winterfeld)/",
         2956,
         [
@@ -792,7 +918,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # GESTA Bereng. 355–401
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/G/GESTA Bereng. (ed. v. Winterfeld)/",
         2428,
         [
@@ -800,7 +926,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # HUCBALD. carm. 265–73
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/H/HUCBALD. carm. (ed. v. Winterfeld)/",
         2528,
         [
@@ -808,7 +934,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # HUCBALD. carm. app. 274–75
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/H/HUCBALD. carm. (app.) (ed. v. Winterfeld)/",
         2957,
         [
@@ -816,7 +942,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # LIOS MON. 278–95
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/L/LIOS MON. (ed. v. Winterfeld)/",
         2586,
         [
@@ -824,7 +950,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # POETA SAXO 7–71
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/P/POETA SAXO (ed. v. Winterfeld)/",
         2641,
         [
@@ -832,7 +958,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # RADBOD. carm. 162–73
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/R/RADBOD. carm. (ed. v. Winterfeld)/",
         2647,
         [
@@ -840,7 +966,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # SALOM. III. carm. I 297–310
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/S/SALOM. III. carm. I (ed. v. Winterfeld)/",
         2678,
         [
@@ -848,7 +974,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # SYLL. Bern. 243–61
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/S/SYLL. Bern. (ed. v. Winterfeld)/",
         2702,
         [
@@ -856,7 +982,7 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
     setup_MGH_ressource( # WALDR. carm. 310–14
-        "https://www.dmgh.de/mgh_poetae_4_1/img/150/mgh_poetae_4_1_",
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
         "/S/SYLL. Bern. (ed. v. Winterfeld)/",
         2857,
         [
@@ -970,10 +1096,51 @@ def import_mgh():
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
 
+def import_mgh(): 
+    setup_MGH_ressource( # O. Holder-Egger MG Script. XV (1887)
+        "https://www.dmgh.de/mgh_ss_15_1/img/300/mgh_ss_15_1_",
+        "/M/MG Script. XV (1887, ed. Holder-Egger)/",
+        2074,
+        [
+            {"img_number": 8, "page_number": 1, "count": 574},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # B. Schmeidler, Adam von Bremen, Hamburgische Kirchengesch. (MG Script. rer. Germ.). 1917.
+        "https://www.dmgh.de/mgh_ss_rer_germ_2/img/300/mgh_ss_rer_germ_2_",
+        "/M/MG Script rer. Germ. (1917, ed. Schmeidler)/",
+        2073,
+        [
+            {"img_number": 68, "page_number": 1, "count": 353},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # F. Kurze, Reginonis ... chronicon (MG Script. rer. Germ.). 1890. p. 154-79
+        "https://www.dmgh.de/mgh_ss_rer_germ_50/img/300/mgh_ss_rer_germ_50_",
+        "/M/MG Script. rer. Germ. (1890, ed. Kurze)/",
+        2071,
+        [
+            {"img_number": 20, "page_number": 1, "count": 196},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # P. v. Winterfeld MG Poet. IV (1899)
+        "https://www.dmgh.de/mgh_poetae_4_1/img/300/mgh_poetae_4_1_",
+        "/M/MG Script. rer. Germ. (1890, ed. v. Winterfeld)/",
+        2067,
+        [
+            {"img_number": 9, "page_number": 1, "count": 444},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # K. Strecker MG Poet. IV (1923)
+        "https://www.dmgh.de/mgh_poetae_4_23/img/300/mgh_poetae_4_23_",
+        "/M/MG Poet IV (1923, ed. Strecker)/",
+        2078,
+        [
+            {"img_number": 11, "page_number": 445, "count": 733},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
 if __name__ == '__main__':
     #converZettelThread = threading.Thread(target=convertZettel, args=(50000,))
     #converZettelThread.start()
     #autoSetZettelType()
-    #import_mgh()
+    import_mgh()
     #print("starting server...")
-    server.start()
+    #server.start()
