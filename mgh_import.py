3bf6ad8444d3c654adfd19b80c42bf6cb82308dc
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2021-12-17 17:47:17 +0100

    Beta 11.9

--- mgh_import.py
+++ mgh_import.py
@@ -49,11 +49,52 @@ def setup_MGH_ressource(basic_url, ressource_path, pages_lst):
     for pages in pages_lst:
         get_MGH_img(pages["img_number"], pages["page_number"], pages["count"], basic_url, ressource_path)
 if __name__ == '__main__':
-    setup_MGH_ressource( # K. Strecker MG Poet. IV (1923)
-        "https://www.dmgh.de/mgh_poetae_4_23/img/300/mgh_poetae_4_23_",
-        "/M/MG Poet IV (1923, ed. Strecker)/",
+    pass
+    #setup_MGH_ressource( # K. Strecker MG Poet. IV (1923)
+    #    "https://www.dmgh.de/mgh_poetae_4_23/img/300/mgh_poetae_4_23_",
+    #    "/M/MG Poet IV (1923, ed. Strecker)/",
+    #    [
+    #        {"img_number": 11, "page_number": 445, "count": 456},
+    #        {"img_number": 471, "page_number": 901, "count": 276},
+    #        {"img_number": 1, "page_number": 9000, "count": 2}
+    #    ])
+
+
+def import_mgh2():
+    # F.-J. Schmale, Adalbertus Samaritanus, Praecepta dictaminum (MG Quellen z. Geistesgesch. d. Mittelalters III). 1961 <- welche seiten?
+    # K. Hampe, Die Aktenstücke z. Frieden von S. Germano (MG Epist. sel. IV). 1926 -> Welche Seiten?
+    # T. Schieffer, Die Urkunden der burgundischen Rudolfinger (MG regum Burgund. e stirpe Rudolf. diplomata et acta). 1977 -> Welche Seiten? https://www.dmgh.de/mgh_dd_rudolf/index.htm#page/92/mode/1up
+    # F. Güterbock (MG Script. rer. Germ. N. S. VII)
+    setup_MGH_ressource( # ACERB. MOR. hist. 130–76
+        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/300/mgh_ss_rer_germ_n_s_7_",
+        "/A/ACERB. MOR. hist. (ed. Güterbock)/",
+        2068,
         [
-            {"img_number": 11, "page_number": 445, "count": 456},
-            {"img_number": 471, "page_number": 901, "count": 276},
+            {"img_number": 175, "page_number": 130, "count": 47},
             {"img_number": 1, "page_number": 9000, "count": 2}
         ])
+    setup_MGH_ressource( # HIST. Frid. imp. cont. 177–218
+        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/300/mgh_ss_rer_germ_n_s_7_",
+        "/H/HIST. Frid. imp. cont. (ed. Güterbock)/",
+        2494,
+        [
+            {"img_number": 222, "page_number": 177, "count": 42},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # HIST. Frid. imp. retr. 1–218 # warum hier auch 218???
+        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/300/mgh_ss_rer_germ_n_s_7_",
+        "/H/HIST. Frid. imp. retr. (ed. Güterbock)/",
+        2495,
+        [
+            {"img_number": 46, "page_number": 1, "count": 218},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+    setup_MGH_ressource( # OTTO MOR. hist. 1–129 # siehe oben???
+        "https://www.dmgh.de/mgh_ss_rer_germ_n_s_7/img/300/mgh_ss_rer_germ_n_s_7_",
+        "/O/OTTO MOR. hist. (ed. Güterbock)/",
+        2631,
+        [
+            {"img_number": 46, "page_number": 1, "count": 129},
+            {"img_number": 1, "page_number": 9000, "count": 2}
+        ])
+ 
