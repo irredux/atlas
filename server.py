42de772d3e91e970cbdc473f98cdd44eb0acae98
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2022-07-06 16:56:57 +0200

    Zettel Upload2

--- server.py
+++ server.py
@@ -459,7 +459,7 @@ def f_zettel(project, letter, dir_nr, img): # NOT SAVE!!!!!!!! NEEDS AUTH
     if cfg["server"]["host"] == "localhost":
         return redirect(f"https://dienste.badw.de:9999/zettel/{letter}/{dir_nr}/{img}", code=302)
     else:
-        return send_file(f"{cfg['projects'][project]['zettel_dir']}/{letter}/{dir_nr}/"+img)
+        return send_file(f"{cfg['projects'][project]['zettel_dir']}/zettel/{letter}/{dir_nr}/{img}")
 @app.route("/<string:project>/file/<res>/<res_id>")
 def file_read(project, res, res_id):
     if res == "scan":
