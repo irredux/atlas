9c470c9e16e87afecbc136913178cd9017f942d6
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2022-07-09 13:06:58 +0200

    Geschichtsquellen

--- docs/updateOperaList.sql
+++ docs/updateOperaList.sql
@@ -3,9 +3,9 @@ CREATE OR REPLACE PROCEDURE updateOperaLists ()
 BEGIN
 /* maiora list */
     /* 1. insert works on separat line */
-    CREATE OR REPLACE TABLE opera_maiora_temp (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT, work_id INT, date_display VARCHAR(200), abbr VARCHAR(200), full TEXT, search VARCHAR(300), bibliography TEXT, comment TEXT, editions_id JSON, editions_url JSON, editions_label JSON, author_sort VARCHAR(200), work_sort VARCHAR(200), gq_id INT);
+    CREATE OR REPLACE TABLE opera_maiora_temp (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT, work_id INT, date_display VARCHAR(200), abbr VARCHAR(200), full TEXT, search VARCHAR(300), bibliography TEXT, comment TEXT, editions_id JSON, editions_url JSON, editions_label JSON, author_sort VARCHAR(200), work_sort VARCHAR(200), gq_author_id INT, gq_work_id INT, same_line BOOL);
     INSERT INTO opera_maiora_temp
-    (work_id, date_display, abbr, full, bibliography, comment, author_sort, work_sort, search, gq_id)
+    (work_id, date_display, abbr, full, bibliography, comment, author_sort, work_sort, search, gq_work_id, gq_author_id)
         SELECT
             work.id,
             work.date_display,
@@ -27,7 +27,8 @@ BEGIN
             author.abbr_sort,
             work.abbr_sort,
             work.ac_web,
-            work.gq_id
+            work.gq_id,
+            author.gq_id
         FROM work
         LEFT JOIN author ON author.id = work.author_id
         WHERE is_maior = 1 AND work.abbr IS NOT NULL
@@ -35,7 +36,7 @@ BEGIN
 
     /* 2. insert works+authors an same line */
     INSERT INTO opera_maiora_temp
-    (author_id, work_id, date_display, abbr, full, bibliography, comment, author_sort, work_sort, search, gq_id)
+    (author_id, work_id, date_display, abbr, full, bibliography, comment, author_sort, work_sort, search, gq_work_id, gq_author_id, same_line)
         SELECT
             author.id,
             work.id,
@@ -58,7 +59,9 @@ BEGIN
             author.abbr_sort,
             work.abbr_sort,
             work.ac_web,
-            author.gq_id
+            work.gq_id,
+            author.gq_id,
+            1
         FROM work
         LEFT JOIN author ON author.id = work.author_id
         WHERE is_maior = 1 AND work.abbr IS NULL
@@ -74,17 +77,17 @@ BEGIN
 
     /* 4. add authors on separat line */
     INSERT INTO opera_maiora_temp
-    (author_id, date_display, abbr, full, comment, author_sort, work_sort, gq_id)
+    (author_id, date_display, abbr, full, comment, author_sort, work_sort, gq_author_id)
         SELECT author.id, author.date_display, IF(author.in_use != 1, CONCAT('[', author.abbr, ']'), author.abbr), author.full, author.txt_info, author.abbr_sort, "", author.gq_id
         FROM author
         WHERE (SELECT COUNT(work.id) FROM work WHERE work.author_id = author.id AND work.is_maior = 1 AND work.abbr IS NOT NULL) > 0 AND (SELECT COUNT(work.id) FROM work WHERE work.author_id = author.id AND work.is_maior = 1 AND work.abbr IS NULL) = 0
     ;
 
     /* 5. sort and insert into final table */
-    CREATE OR REPLACE TABLE opera_maiora (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT, work_id INT, date_display VARCHAR(200), abbr VARCHAR(200), full TEXT, search VARCHAR(300), bibliography TEXT, comment TEXT, editions_id JSON, editions_url JSON, editions_label JSON, gq_id INT);
+    CREATE OR REPLACE TABLE opera_maiora (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT, work_id INT, date_display VARCHAR(200), abbr VARCHAR(200), full TEXT, search VARCHAR(300), bibliography TEXT, comment TEXT, editions_id JSON, editions_url JSON, editions_label JSON, gq_author_id INT, gq_work_id INT, same_line BOOL);
     INSERT INTO opera_maiora
-    (author_id, work_id, date_display, abbr, full, search, bibliography, comment, editions_id, editions_url, editions_label, gq_id)
-        SELECT author_id, work_id, date_display, abbr, full, search, bibliography, comment, editions_id, editions_url, editions_label, gq_id
+    (author_id, work_id, date_display, abbr, full, search, bibliography, comment, editions_id, editions_url, editions_label, gq_work_id, gq_author_id, same_line)
+        SELECT author_id, work_id, date_display, abbr, full, search, bibliography, comment, editions_id, editions_url, editions_label, gq_work_id, gq_author_id, same_line
         FROM opera_maiora_temp
         ORDER BY opera_maiora_temp.author_sort, opera_maiora_temp.work_sort
     ;
@@ -99,9 +102,9 @@ BEGIN
 
 
 /* opera minora */
-    CREATE OR REPLACE TABLE opera_minora_temp (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT, work_id INT, date_display VARCHAR(200), citation TEXT, bibliography TEXT, search VARCHAR(300), editions_id JSON, editions_url JSON, editions_label JSON, gq_id INT);
+    CREATE OR REPLACE TABLE opera_minora_temp (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT, work_id INT, date_display VARCHAR(200), citation TEXT, bibliography TEXT, search VARCHAR(300), editions_id JSON, editions_url JSON, editions_label JSON, gq_author_id INT, gq_work_id INT);
 
-    INSERT INTO opera_minora_temp(author_id, work_id, date_display, citation, bibliography, search, gq_id) SELECT author.id, work.id, work.date_display, CONCAT(IF(work.in_use = 0, "<del>", ""), REPLACE(work.opus, " <cit></cit> ( <cit_bib></cit_bib>)", ""), IF(work.in_use = 0, "</del>", ""), IF(work.reference IS NOT NULL AND work.reference != "", CONCAT("&nbsp;&nbsp;&nbsp;<i>v.</i> ", work.reference), "")), work.txt_info, work.ac_web, author.gq_id FROM work LEFT JOIN author ON author.id = work.author_id WHERE is_maior = 0 ORDER By author.abbr_sort, work.abbr_sort;
+    INSERT INTO opera_minora_temp(author_id, work_id, date_display, citation, bibliography, search, gq_work_id, gq_author_id) SELECT author.id, work.id, work.date_display, CONCAT(IF(work.in_use = 0, "<del>", ""), REPLACE(work.opus, " <cit></cit> ( <cit_bib></cit_bib>)", ""), IF(work.in_use = 0, "</del>", ""), IF(work.reference IS NOT NULL AND work.reference != "", CONCAT("&nbsp;&nbsp;&nbsp;<i>v.</i> ", work.reference), "")), work.txt_info, work.ac_web, work.gq_id, author.gq_id FROM work LEFT JOIN author ON author.id = work.author_id WHERE is_maior = 0 ORDER By author.abbr_sort, work.abbr_sort;
     UPDATE opera_minora_temp SET
         editions_id = CONCAT('[',(SELECT GROUP_CONCAT(id SEPARATOR ',') FROM edition WHERE edition.work_id = opera_minora_temp.work_id),']');
     UPDATE opera_minora_temp SET
@@ -109,10 +112,10 @@ BEGIN
     UPDATE opera_minora_temp SET
         editions_label = CONCAT('["',(SELECT GROUP_CONCAT(IF(label IS NULL, "FEHLER", label) SEPARATOR '","') FROM edition WHERE edition.work_id = opera_minora_temp.work_id),'"]');
 
-    CREATE OR REPLACE TABLE opera_minora (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT, work_id INT, date_display VARCHAR(200), citation TEXT, bibliography TEXT, search VARCHAR(300), editions_id JSON, editions_url JSON, editions_label JSON, gq_id INT);
+    CREATE OR REPLACE TABLE opera_minora (id INT PRIMARY KEY AUTO_INCREMENT, author_id INT, work_id INT, date_display VARCHAR(200), citation TEXT, bibliography TEXT, search VARCHAR(300), editions_id JSON, editions_url JSON, editions_label JSON, gq_author_id INT, gq_work_id INT);
     INSERT INTO opera_minora
-    (author_id, work_id, date_display, citation, bibliography, search, editions_id, editions_url, editions_label, gq_id)
-        SELECT author_id, work_id, date_display, citation, bibliography, search, editions_id, editions_url, editions_label, gq_id
+    (author_id, work_id, date_display, citation, bibliography, search, editions_id, editions_url, editions_label, gq_work_id, gq_author_id)
+        SELECT author_id, work_id, date_display, citation, bibliography, search, editions_id, editions_url, editions_label, gq_work_id, gq_author_id
         FROM opera_minora_temp
     ;
     DROP TABLE opera_minora_temp;
