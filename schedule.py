637c40c8189588716cde78d915d4b570dbec190d
Alexander Häberlin <alexander.haeberlin@mlw.badw.de>
2022-07-28 17:39:35 +0200

    schedule fix

--- schedule.py
+++ schedule.py
@@ -1,6 +1,5 @@
 #!/user/bin/env python3
 # -*- coding: utf-8 -*-
-#from configparser import ConfigParser
 from os import path
 from sys import argv
 import json
@@ -8,9 +7,6 @@ from arachne import Arachne
 from archimedes import Archimedes
 
 dir_path = path.dirname(path.abspath(__file__))
-#cfg = ConfigParser()
-#cfg.read(dir_path+"/config/dienstrechner.ini")
-#cfg_file_name = argv[1] if len(argv) > 1 else dir_path+"/config/localhost.json"
 with open(dir_path+"/config/dienstrechner.json", "r") as cfg_file: cfg = json.load(cfg_file)
 
 db = Arachne(cfg["projects"]["mlw"]["database"])
@@ -22,4 +18,4 @@ auto = Archimedes(db, dir_path)
 
 # ocr and ML
 #auto.convertZettel(50000)
-auto.ocr_scan(10, False)
+auto.ocr_scan(2000, False)
